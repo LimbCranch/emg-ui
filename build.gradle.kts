29c719615588c54e6fa3a43581f09114aa2e288f
DenisBogusevich <bogusevich03@gmail.com>
2025-06-12 23:42:21 +0200

    add Dokka configuration for API documentation generation

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,45 @@ plugins {
     id("com.android.application") version "8.1.2" apply false
     id("com.android.library") version "8.1.2" apply false
     id("org.jetbrains.compose") version "1.5.4" apply false
+    id("org.jetbrains.dokka") version "1.9.10"
+
+}
+
+dokka {
+    outputDirectory.set(file("$buildDir/dokka"))
+
+    dokkaSourceSets {
+        named("commonMain") {
+            displayName.set("Common")
+
+            sourceLink {
+                localDirectory.set(file("src/commonMain/kotlin"))
+                remoteUrl.set(URL("https://github.com/emg-prosthetics/emg-ui/tree/main/common/src/commonMain/kotlin"))
+                remoteLineSuffix.set("#L")
+            }
+
+            perPackageOption {
+                matchingRegex.set(".*\\.internal.*")
+                suppress.set(true)
+            }
+
+            externalDocumentationLink {
+                url.set(URL("https://kotlinlang.org/api/latest/jvm/stdlib/"))
+            }
+        }
+    }
+}
+
+
+tasks.register("generateApiDocs") {
+    dependsOn("dokkaHtml")
+
+    doLast {
+        copy {
+            from("$buildDir/dokka")
+            into("../emg-docs/docs/api/kotlin/")
+        }
+    }
 }
 
 allprojects {
